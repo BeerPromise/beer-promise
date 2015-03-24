667630fe33ba6daea46a2151830d2a910f93b22a
Erik Griffin <griffin.erik@gmail.com>
2015-03-24 14:58:59 +0000

    Added Procfile. Fixed Heroku port number

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var port = process.env.PORT || 3000;
 
-server.listen(3000, function(){
-  console.log("Listening on server port 3000");
+server.listen(port, function(){
+  console.log("Listening on server port "+ port);
 });
 
 app.get('/', function(request, response){
