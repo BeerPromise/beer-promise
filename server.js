9cb8cf6e64644a04c72afd27240d7ef0dec39764
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-24 16:18:00 +0000

    mongo, body-parser and mongojs

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
+var mongojs = require('mongojs');
+var db = mongojs((process.env.MONGOLAB_URI || 'beer-promise'), ['beer-promise']);
+var bodyParser = require('body-parser');  // pull information from HTML POST (express4)
+
+app.set('views', __dirname + '/public');
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 server.listen(port, function(){
   console.log("Listening on server port "+ port);
