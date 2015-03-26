c147587ea802a52db3c4f47d3b34c3b2618e8514
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-26 12:28:35 +0000

    error

--- specs/features/user_management.spec.js
+++ specs/features/user_management.spec.js
@@ -12,6 +12,17 @@ var db = mongojs(('beer-testing'), ['customers', 'bars', 'menus']);
     expect(browser.getTitle()).toEqual('Beer Promise');
   });
 
+  it('can create a user', function(){
+    browser.get('http://localhost:3000');
+    var email = element(by.id('email-sign-up'));
+    var password = element(by.id('password-sign-up'));
+    var submit = element(by.id('sign-up-submit'));
+    email.sendKeys('test@makers.com');
+    password.sendKeys('123456');
+    submit.click();
+    expect(element(by.id('welcome')).getText()).toContain('welcome, test@makers.com');
+  });
+
 
 
 
