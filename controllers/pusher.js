26d3acb6cc3c51b749f4febe67bf42bfb78f0111
Erik Griffin <griffin.erik@gmail.com>
2015-03-27 02:47:39 +0000

    Hahaha wow.  The angular clients were still connected to the localhost app. So clicking on the dev environment updated the staging website.

--- controllers/pusher.js
+++ controllers/pusher.js
@@ -8,17 +8,16 @@ try {
 } catch(e) {
   console.log('Local secrets not found, using Heroku');
   pusher = Pusher.forURL(process.env.PUSHER_URL);
-  console.log('!!!!! '+process.env.PUSHER_URL);
 }
 
 if (secrets) {
-  var pusher = new Pusher({
+  pusher = new Pusher({
     appId: secrets.pusherAppId,
     key: secrets.pusherKey,
     secret: secrets.pusherSecret
   });
 }
-console.log('pusher is '+JSON.stringify(pusher));
+
 module.exports = pusher;
 
 
