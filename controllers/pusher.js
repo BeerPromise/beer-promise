8f7cc291f149005248230ef985ce238b11072196
Erik Griffin <griffin.erik@gmail.com>
2015-03-27 02:38:23 +0000

    This should work

--- controllers/pusher.js
+++ controllers/pusher.js
@@ -1,25 +1,23 @@
-var secrets;
 var Pusher = require('pusher');
+var secrets;
+var pusher;
+
+
 try {
   secrets = require('./secrets');
 } catch(e) {
   console.log('Local secrets not found, using Heroku');
-  console.log('---- '+process.env.PUSHER_APP_ID);
-  console.log('==== '+process.env.PUSER_ID);
-  console.log('==== '+process.env.PUSHER_KEY);
-  console.log('============================');
-  console.log('++++ '+process.env['PUSHER_KEY']);
-  secrets = {
-    pusherAppId: process.env.PUSHER_APP_ID,
-    pusherKey: process.env.PUSHER_KEY,
-    pusherSecret: process.env.PUSHER_SECRET
-  };
+  pusher = Pusher.forURL(process.env.PUSHER_URL);
 }
 
-var pusher = new Pusher({
-  appId: secrets.pusherAppId,
-  key: secrets.pusherKey,
-  secret: secrets.pusherSecret
-});
+if (secrets) {
+  var pusher = new Pusher({
+    appId: secrets.pusherAppId,
+    key: secrets.pusherKey,
+    secret: secrets.pusherSecret
+  });
+}
 
 module.exports = pusher;
+
+
