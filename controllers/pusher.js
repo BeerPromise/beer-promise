bc4709b791b2cc5378aa02a6d35f5e943c31debd
Erik Griffin <griffin.erik@gmail.com>
2015-03-27 02:15:02 +0000

    Debugging heroku

--- controllers/pusher.js
+++ controllers/pusher.js
@@ -4,6 +4,7 @@ try {
   secrets = require('./secrets');
 } catch(e) {
   console.log('Local secrets not found, using Heroku');
+  console.log('---- '+process.env.PUSHER_APP_ID);
   secrets = {
     pusherAppId: process.env.PUSHER_APP_ID,
     pusherKey: process.env.PUSHER_KEY,
