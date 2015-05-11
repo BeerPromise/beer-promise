6d30b05623912a326c8d6666c8e5aed3228dcdca
Sean Haughton <seanphaughton@gmail.com>
2015-05-11 14:34:43 +0100

    Added bcrypt setup

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.set('view engine', 'ejs');
 
 // --- Database --
 var mongojs = require('mongojs');
-var db = mongojs('todolist', ['todolist']);
+var db = mongojs((process.env.MONGOLAB_URI || 'beer-'+process.env.BEER_NODE_ENV), ['customers', 'bars', 'menus']);
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({'extended':'true'}));
 
 // -- Server Start --
 server.listen(port, function(){
