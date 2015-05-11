131435563aedf40f6869ffb25fa96019ece52f25
Julian Veling <julian_veling@hotmail.com>
2015-05-11 16:12:31 +0100

    jasmine test

---
+++ spec/frontSpec.js
@@ -0,0 +1,11 @@
+describe('Golden Ticket', function(){
+
+  describe('index', function(){
+    it('should have a title', function(){
+      browser.get('/')
+      expect(browser.getTitle()).toEqual('Beer Promise')
+    });
+
+
+  });
+});
