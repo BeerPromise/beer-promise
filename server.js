3f8f51657e340137181d884a77445ba1d70b4829
Julian Veling <julian_veling@hotmail.com>
2015-05-11 15:57:03 +0100

    files

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
-var root = __dirname + '/public/';
 app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
 
 // --- Database --
 var mongojs = require('mongojs');
-var db = mongojs((process.env.MONGOLAB_URI || 'beer-'+process.env.BEER_NODE_ENV), ['customers', 'bars', 'menus']);
+var db = mongojs((process.env.MONGOLAB_URI || 'beer-' + process.env.BEER_NODE_ENV), ['customers', 'bars', 'menus']);
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({'extended':'true'}));
 
@@ -20,3 +18,10 @@ var bcrypt = require('./controllers/bcrypt');
 server.listen(port, function(){
   console.log("Listening on server port " + port);
 });
+
+app.get('/', function(req, res) {
+  // This does NOT get called if there is an index.html...
+  res.render('index')
+});
+
+module.exports = server;
