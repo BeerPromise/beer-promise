173e6f91c83a2e15f1f23a8e2fb849cee430f34f
Julian Veling <julian_veling@hotmail.com>
2015-05-11 13:02:50 +0100

    npm

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ var app = express();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 var root = __dirname + '/public/';
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public'));
-app.use(methodOverride());
 app.set('view engine', 'ejs');
 
 // --- Database --
